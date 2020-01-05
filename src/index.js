d3a002b34e0564e4caba0c88c2d4e19c6bba2df4
nick_cher <nick_cher@list.ru>
2020-01-05 20:48:04 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import TopBar from "components/topBar";
+import Routes from "pages/routes";
+import { CurrentUserProvider } from "contexts/currentUser";
+import CurrentUserChecker from "components/CurrentUserChecker";
+
+const App = () => {
+  return (
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
