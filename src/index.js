8fb2f4013662234684e2a0a1604e6df58e0778fe
nick_cher <nick_cher@list.ru>
2020-01-16 21:23:21 +0300

    remove everything

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import TopBar from "components/TopBar";
-import Routes from "pages/routes";
-import { CurrentUserProvider } from "contexts/currentUser";
-import CurrentUserChecker from "components/CurrentUserChecker";
 import './App.css';
 
 const App = () => {
